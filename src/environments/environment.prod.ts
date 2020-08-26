3146090aae0c28892eff452d2e65a082b7e30f47
Eric Acosta Vega~ <eacosta901@gmail.com>
2020-08-26 17:43:55 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "XXXXXXXXXXx",
+    authDomain: "ingreso-egreso-app-2f2b3.firebaseapp.com",
+    databaseURL: "https://ingreso-egreso-app-2f2b3.firebaseio.com",
+    projectId: "ingreso-egreso-app-2f2b3",
+    storageBucket: "ingreso-egreso-app-2f2b3.appspot.com",
+    messagingSenderId: "943452000441",
+    appId: "XXXXXXXXXXXXX8"
+
+  }
+};
